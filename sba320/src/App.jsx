48c5d0bb234eb504248c2b5899692a0e6d7ba484
Aziz <azizatamuradov1994@gmail.com>
2024-09-28 12:22:59 -0400

    components

--- sba320/src/App.jsx
+++ sba320/src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
- 
+import {Route, Routes} from "react-router-dom" 
 import './App.css'
 
 function App() {
-
+  
 
   return (
     <>
-     
+     <Routes>
+
+      <Route path='/movies' element={<Movies/>}/>
+      <Route path='/cartoons' element={<Cartoons/>} />
+      <Route path='/news' element={<News/>} />
+      
+     </Routes>
     </>
   )
 }
